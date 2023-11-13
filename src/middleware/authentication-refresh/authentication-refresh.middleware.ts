10757cf12ccef60155fbbb83e5251d90a6d723cd
geekmorn <geekmorn@pm.me>
2023-11-13 23:58:31 +0100

    :heavy_plus_sign: Added new middleware especial for refresh endpoint

---
+++ src/middleware/authentication-refresh/authentication-refresh.middleware.ts
@@ -0,0 +1,34 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { HttpService } from 'services/http';
+import { JwtService } from 'services/jwt';
+import { RequestService } from 'services/request';
+import { ValidatorService } from 'services/validator';
+import { AccessHeaderStrategy } from 'services/validator/strategies';
+
+@Injectable()
+export class AuthenticationRefreshMiddleware implements NestMiddleware {
+  constructor(
+    private requestService: RequestService,
+    private jwtService: JwtService,
+    private httpService: HttpService,
+    private validator: ValidatorService,
+  ) {
+    this.validator.attachStrategies([new AccessHeaderStrategy()]);
+  }
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const access = await this.httpService.extractHeader('authorization', req);
+    const refresh = await this.httpService.extractCookie('refreshToken', req);
+    const validatedAccess = await this.validator.validate(
+      'accessHeader',
+      access,
+    );
+
+    const userId = await this.jwtService.decode(validatedAccess);
+    this.requestService.userId = userId;
+    this.requestService.refreshToken = refresh;
+
+    next();
+  }
+}
